a6a8dbee6428a2cd61bddd4aab7b47a894361b36
Marc De Jesus <marcantoniodejesus@yahoo.com>
2025-08-09 12:58:26 -0400

    Volunteer page

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -80,7 +80,7 @@ export default function VolunteerPage() {
 
       <div className="mt-20">
         <h2 className="text-3xl font-bold font-heading text-center mb-8">Additional Volunteer Opportunities</h2>
-        <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {additionalOpportunities.map((opp) => (
                   <Card key={opp.title} className="p-4">
                     <div className="flex items-center space-x-4">
